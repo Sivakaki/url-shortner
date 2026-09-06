75d6ca1960c00fabd5c19b58e079408eca60b6f0
Sivakaki <sivakaki033@gmail.com>
2026-09-06 18:25:26 +0530

    add user and url models

---
+++ src/models/url.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const urlSchema = new Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Url = mongoose.model("Url", urlSchema);
